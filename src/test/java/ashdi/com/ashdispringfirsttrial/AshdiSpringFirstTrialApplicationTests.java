674850bccefaf43704928db0200899d7a37e8ed5
pro100 <frostymag177@gmail.com>
2022-01-22 15:56:03 +0300

    Initial commit

---
+++ src/test/java/ashdi/com/ashdispringfirsttrial/AshdiSpringFirstTrialApplicationTests.java
@@ -0,0 +1,13 @@
+package ashdi.com.ashdispringfirsttrial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AshdiSpringFirstTrialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
