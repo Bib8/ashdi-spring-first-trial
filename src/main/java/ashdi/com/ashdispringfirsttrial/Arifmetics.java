84d2b1ee951fa7d23daefc6deb263f6b8d0bda60
pro100 <frostymag177@gmail.com>
2022-01-22 15:56:53 +0300

    add res

---
+++ src/main/java/ashdi/com/ashdispringfirsttrial/Arifmetics.java
@@ -0,0 +1,16 @@
+package ashdi.com.ashdispringfirsttrial;
+
+public class Arifmetics {
+    public final int sumResult (int a,int b){
+        return a + b;
+    }
+    public final int minusResult (int a, int b){
+        return a - b;
+    }
+    public final int multiplyResult (int a, int b){
+        return a * b;
+    }
+    public final int divideResult (int a, int b){
+        return a / b;
+    }
+}
