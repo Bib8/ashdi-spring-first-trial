758b02cac28eef6203e2133057d9462dcaffd700
pro100 <frostymag177@gmail.com>
2022-03-12 22:39:43 +0300

    add ref

--- src/test/java/ashdi/com/ashdispringfirsttrial/CalcServiceImplTest.java
+++ src/test/java/ashdi/com/ashdispringfirsttrial/CalcServiceImplTest.java
@@ -11,39 +11,39 @@ public class CalcServiceImplTest {
 
     @Test
     //expected R = 3, of sum num 2,1
-    public void shouldСompareExpectedResRFndActualResATestSum(){
+    public void shouldCompareExpectedResRFndActualResATestSum(){
         Assert.assertEquals(THREE, out.sumResult(TWO, ONE));
     }
     //expected R = 0, of sum num -1,1
-    public void shouldСompareExpectedResRFndActualResATestSumNegative(){
+    public void shouldCompareExpectedResRFndActualResATestSumNegative(){
         Assert.assertEquals(ZERO, out.sumResult(NEGATIVE_ONE, ONE));
     }
     //expected R = 0, of sum num 1,1
-    public void shouldСompareExpectedResRFndActualResADiff(){
+    public void shouldCompareExpectedResRFndActualResADiff(){
         Assert.assertEquals(ZERO, out.minusResult(ONE, ONE));
     }
     //expected R = -2, of sum num -1,1
-    public void shouldСompareExpectedResRFndActualResADiffNegative(){
+    public void shouldCompareExpectedResRFndActualResADiffNegative(){
         Assert.assertEquals(NEGATIVE_TWO, out.minusResult(NEGATIVE_ONE, ONE));
     }
     //expected R = 3, of multiplying, factor 2,1
-    public void shouldСompareExpectedResRFndActualResAMultiply(){
-        Assert.assertEquals(THREE, out.multiplyResult(TWO, THREE));
+    public void shouldCompareExpectedResRFndActualResAMultiply(){
+        Assert.assertEquals(THREE, out.multiplyResult(TWO, ONE));
     }
     //expected R = 0, of multiplying, factor 2,0
-    public void shouldСompareExpectedResRFndActualResAMultiplyZero(){
+    public void shouldCompareExpectedResRFndActualResAMultiplyZero(){
         Assert.assertEquals(ZERO, out.multiplyResult(TWO, ZERO));
     }
     //expected R = 1, of private, divider 3,3
-    public void shouldСompareExpectedResRFndActualResAdivide() throws ZeroDivideException {
+    public void shouldCompareExpectedResRFndActualResAdivide() throws ZeroDivideException {
         Assert.assertEquals(THREE, out.divideResult(THREE, THREE));
     }
     //expected R = -1, of private, divider 3,-3
-    public void shouldСompareExpectedResRFndActualResAdivideNegative() throws ZeroDivideException {
+    public void shouldCompareExpectedResRFndActualResAdivideNegative() throws ZeroDivideException {
         Assert.assertEquals(THREE, out.divideResult(THREE, NEGATIVE_THREE));
     }
     //expected R = throw Exception private, divider 3,0
-   public void shouldСompareExpectedResRFndActualResAdivideZeroException(){
+   public void shouldCompareExpectedResRFndActualResAdivideZeroException(){
        Assert.assertThrows(ZeroDivideException.class, () -> out.divideResult(THREE, ZERO));
     }
 
